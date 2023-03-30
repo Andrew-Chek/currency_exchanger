e313aee74f55a0c38f4fb25f9a43d853f2e5e55c
Andrew-Chek <andrii.chekyrda@gmail.com>
2023-03-30 11:54:33 +0300

    init app [add] - main structure created

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExchangeComponent } from './features/exchange/exchange.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExchangeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
